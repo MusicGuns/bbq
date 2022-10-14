a50f667b50c93930e8198112b9b94f7da31af3e0
MusicGun <battcamm@mail.ru>
2022-10-14 22:08:02 +0300

    smtp

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -3,7 +3,7 @@
 # Базовый класс для всех мэйлеров приложения
 class ApplicationMailer < ActionMailer::Base
   # обратный адрес всех писем по умолчанию
-  default from: ENV["MAILJET_SENDER"]
+  default from: ENV["MAIL_SENDER"]
 
   # Задаем макет для всех писем
   layout 'mailer'
