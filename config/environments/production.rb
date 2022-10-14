d2f09e07e93a222c8f26280a582c9fa0d08e4494
MusicGun <battcamm@mail.ru>
2022-10-14 12:33:44 +0300

    mailjet

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,5 +80,11 @@ Rails.application.configure do
   # Базовый урл сайта, для генерации правильных ссылок в письмах
   # ПРОПИСЫВАЙТЕ свой!
   config.action_mailer.default_url_options = {host: 'mega-bbq123.herokuapp.com'}
+
+  # Вываливать ли посетителю сайта ошибки при отправке писем
+  config.action_mailer.raise_delivery_errors = true
+
+  # Делать рассылку писем (при false приложение только имитирует отправку)
+  config.action_mailer.perform_deliveries = false
   config.action_mailer.delivery_method = :mailjet
 end
