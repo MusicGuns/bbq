bd57227360205603bf75bb0bd7b87ef99fdf0fc5
MusicGun <battcamm@mail.ru>
2022-10-28 18:13:51 +0300

    capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server 'bbqmaker.space', user: 'deploy', roles: %w{www db web}
+server 'bbqmaker.space', user: 'deploy', roles: %w{app db web}
 
 # role-based syntax
 # ==================
